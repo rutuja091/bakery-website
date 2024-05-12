83859366c7ba43cfdaa4ae841ef7b13a1d9adaa5
rutuja091 <gaikwadrutuja254@gmail.com>
2024-05-12 09:56:33 +0530

    menu page edit"

--- js/index.js
+++ js/index.js
@@ -1,41 +1,8 @@
-document.getElementById('image1').addEventListener('click', function() {
-    showInfo('Image 1', 'This is the information for Image 1.');
-  });
-  
-  document.getElementById('image2').addEventListener('click', function() {
-    showInfo('Image 2', 'This is the information for Image 2.');
-  });
-  
-  document.getElementById('image3').addEventListener('click', function() {
-    showInfo('Image 3', 'This is the information for Image 3.');
-  });
-  document.getElementById('image4').addEventListener('click', function() {
-    showInfo('Image 4', 'This is the information for Image 1.');
-  });
-  
-  document.getElementById('image5').addEventListener('click', function() {
-    showInfo('Image 5', 'This is the information for Image 2.');
-  });
-  
-  document.getElementById('image6').addEventListener('click', function() {
-    showInfo('Image 6', 'This is the information for Image 3.');
-  });
-  document.getElementById('image7').addEventListener('click', function() {
-    showInfo('Image 7', 'This is the information for Image 1.');
-  });
-  
-  document.getElementById('image8').addEventListener('click', function() {
-    showInfo('Image 8', 'This is the information for Image 2.');
-  });
-  
-  document.getElementById('image9').addEventListener('click', function() {
-    showInfo('Image 9', 'This is the information for Image 3.');
-  });
-  
-  
-  function showInfo(title, text) {
-    document.getElementById('infoTitle').innerText = title;
-    document.getElementById('infoText').innerText = text;
-    document.getElementById('infoContainer').style.display = 'block';
-  }
-  
+function showImageInfo(infoId) {
+    var imageInfo = document.getElementById(infoId);
+    if (imageInfo.style.display === "none" || !imageInfo.style.display) {
+        imageInfo.style.display = "block";
+    } else {
+        imageInfo.style.display = "none";
+    }
+}
