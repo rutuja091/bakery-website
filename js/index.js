a499edfaf429f795ea6956712c250f8abb04860e
rutuja091 <gaikwadrutuja254@gmail.com>
2024-05-14 18:48:59 +0530

    add

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-function showImageInfo(infoId) {
-    var imageInfo = document.getElementById(infoId);
-    if (imageInfo.style.display === "none" || !imageInfo.style.display) {
-        imageInfo.style.display = "block";
-    } else {
-        imageInfo.style.display = "none";
+function change(path) {
+    imgElement = document.getElementById('product-main-image');
+
+    if (path == 1) {
+        imgElement.src = "./images/index/review-one.jpg";
+    }
+    else if (path == 2) {
+        imgElement.src = "./images/index/review-two.png";
+    }
+    else if (path == 3) {
+        imgElement.src = "./images/index/review-three.jpg";
+    }
+    else if (path == 4) {
+        imgElement.src = "./images/index/review-four.jpg";
+    }
+    else if (path == 5) {
+        imgElement.src = "./images/index/review-five.jpg";
+    }
+    else if (path == 6) {
+        imgElement.src = "./images/index/review-six.jpg";
     }
 }
