87e23930ef194b3c1b705cf620eb85c0e1778a3f
rutuja091 <gaikwadrutuja254@gmail.com>
2024-05-20 16:20:41 +0530

    added product page

---
+++ js/product-5.js
@@ -0,0 +1,34 @@
+
+//Product Card Quantity Counter//
+const quantityElement = document.getElementById("quantity");
+function increment() {
+    let quantity = parseInt(quantityElement.innerText);
+    const showQuantity = document.getElementById("show-quantity");
+    const showTotal = document.getElementById("show-total");
+    if (quantity < 10) {
+        quantityElement.innerText = ++quantity;
+    }
+    else {
+        alert("You can't Add more than 10 item!")
+    }
+    showQuantity.innerText = `${quantity} x 20`;
+    showTotal.innerText=`Total: ${quantity * 20} `
+
+}
+function decrement() {
+    let quantity = parseInt(quantityElement.innerText);
+    const showQuantity = document.getElementById("show-quantity");
+    const showTotal = document.getElementById("show-total");
+    if (quantity > 1) {
+        quantityElement.innerText = --quantity;
+    }
+    else {
+        alert("You can't Add less than 1 item!")
+    }
+    showQuantity.innerText = `${quantity} x 20`;
+    showTotal.innerText=`Total: ${quantity * 20}  `
+
+}
+function add(){
+    alert("Your item is successfully added!")
+    }
